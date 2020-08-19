ea2fc5f8ec94f9b124f062dac81b8521deae71da
qg0 <919099+qg0@users.noreply.github.com>
2020-08-19 16:19:01 +0700

    Update get_maximum_amount_of_active_sessions.sql

--- get_maximum_amount_of_active_sessions.sql
+++ get_maximum_amount_of_active_sessions.sql
@@ -12,32 +12,57 @@ SELECT `AMOUNT` FROM
 (
 	SELECT *, @CNT:=@CNT+CNT AS AMOUNT FROM
 	(
-		SELECT `Left` as `Date`, @INC AS CNT FROM `session`
-		UNION
-		SELECT `Right` as `Date`, @DEC AS CNT FROM `session`
+	    SELECT `login_time` as `time`, @INC AS CNT FROM `session` WHERE `login_time` >= @login_time 
+	    UNION
+	    SELECT `logout_time` as `time`, @DEC AS CNT FROM `session`  WHERE `login_time` >= @login_time AND `logout_time` <= @logout_time
 	) T1
-	ORDER BY `Date`
-) T2
+	ORDER BY `time`
+	) T2
 ORDER BY `AMOUNT` DESC LIMIT 1
 
 --------------------------------------------------------------------
 
+--
+-- Table structure for table `session`
+--
+
 CREATE TABLE `session` (
   `id` int(6) UNSIGNED NOT NULL,
-  `Left` date NOT NULL,
-  `Right` date NOT NULL
+  `login_time` datetime NOT NULL,
+  `logout_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO `session` (`id`, `Left`, `Right`) VALUES
-(1, '2010-10-10', '2010-10-20'),
-(2, '2010-10-15', '2010-10-25'),
-(3, '2010-10-05', '2010-10-15'),
-(4, '2010-10-17', '2010-10-28'),
-(5, '2015-10-10', '2016-10-10'),
-(6, '2017-10-10', '2018-10-10'),
-(7, '2010-10-07', '2010-10-08'),
-(8, '2010-10-06', '2010-10-07');
+--
+-- Dumping data for table `session`
+--
 
+INSERT INTO `session` (`id`, `login_time`, `logout_time`) VALUES
+(1, '2010-10-10 00:00:00', '2010-10-20 00:00:00'),
+(2, '2010-10-15 00:00:00', '2010-10-25 00:00:00'),
+(3, '2010-10-05 00:00:00', '2010-10-15 00:00:00'),
+(4, '2010-10-17 00:00:00', '2010-10-28 00:00:00'),
+(5, '2015-10-10 00:00:00', '2016-10-10 00:00:00'),
+(6, '2017-10-10 00:00:00', '2018-10-10 00:00:00'),
+(7, '2010-10-07 00:00:00', '2010-10-08 00:00:00'),
+(8, '2010-10-06 00:00:00', '2010-10-07 00:00:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `session`
+--
 ALTER TABLE `session`
   ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `session`
+--
+ALTER TABLE `session`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
